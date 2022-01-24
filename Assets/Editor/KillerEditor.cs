e08b8defcf70262b10d64b01a711e6c1ef134a06
Kiwinblue <73108961+Kiwinblue@users.noreply.github.com>
2022-01-24 15:00:56 +0100

    Killer v03

--- Assets/Editor/KillerEditor.cs
+++ Assets/Editor/KillerEditor.cs
@@ -12,5 +12,12 @@ public class KillerEditor : Editor
         Killer kllr = (Killer)target;
         Handles.color = Color.white;
         Handles.DrawWireDisc(kllr.transform.position, Vector3.up, kllr.hearingRange);
+
+        Handles.color = Color.blue;
+        Vector3 corner1 = new Vector3(kllr.areaXMinMax.x, 0,kllr.areaZMinMax.x);
+        Vector3 corner2 = new Vector3(kllr.areaXMinMax.y, 0, kllr.areaZMinMax.x);
+        Vector3 corner3 = new Vector3(kllr.areaXMinMax.y, 0, kllr.areaZMinMax.y);
+        Vector3 corner4 = new Vector3(kllr.areaXMinMax.x, 0, kllr.areaZMinMax.y);
+        Handles.DrawAAPolyLine(corner1, corner2, corner3, corner4, corner1);
     }
 }
--- Assets/Scripts/Killer.cs
+++ Assets/Scripts/Killer.cs
@@ -6,36 +6,58 @@ using System;
 
 public class Killer : MonoBehaviour {
 
-    public float hearingRange;
-    
+    public float hearingRange = 20f;
+    public Vector2 areaXMinMax, areaZMinMax;
+    public float destinationThreshold = 2f;
+    public float checkTime = 4f;
+
     NavMeshAgent agent;
+    bool isHunting = false;
 
     private void Awake()
     {
+        //Get Agent
         agent = GetComponent<NavMeshAgent>();
-        
+        //Initialize Movement
+        agent.SetDestination(RandomDestination());
     }
-
     private void Update()
     {
-
+        Movement();
     }
 
-    void Roam()
+    void Movement()
     {
-
+        if (agent.remainingDistance <= destinationThreshold && !isHunting)
+        {
+            agent.SetDestination(RandomDestination());
+            print("direction changed");
+        }
+        else if(agent.remainingDistance <= destinationThreshold && isHunting)
+        {
+            Check();
+        }
     }
-    void Hunting(Vector3 playerPos)
+    void Hunting(Vector3 soundPos)
     {
-        agent.SetDestination(playerPos);
+        if (Vector3.Distance(soundPos, transform.position) <= hearingRange)
+        {
+            agent.SetDestination(soundPos);
+            isHunting = true;
+        }
     }
     void Check()
     {
+        agent.isStopped = true;
 
     }
     void Kill()
     {
 
     }
-
+    Vector3 RandomDestination()
+    {
+        Vector3 newRandomDestination = new Vector3(UnityEngine.Random.Range(areaXMinMax.x, areaXMinMax.y), 0, UnityEngine.Random.Range(areaZMinMax.x, areaZMinMax.y));
+        return newRandomDestination;
+    }
 }
